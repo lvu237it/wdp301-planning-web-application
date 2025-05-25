453addc4cfc06234358237d820013bac962873a2
lvu237it <luuvanvua7k16vt@gmail.com>
2025-05-25 16:51:50 +0700

    Add notes for activityLog model

--- back-end/models/activityLogModel.js
+++ back-end/models/activityLogModel.js
@@ -13,7 +13,14 @@ const activityLogSchema = new mongoose.Schema(
       required: true,
     },
     action: { type: String, required: true },
+    // targetId: Là một ObjectId tham chiếu đến một document cụ thể. (ở đây có thể tham chiếu tới nhiều collection)
     targetId: { type: mongoose.Schema.Types.ObjectId },
+    /*
+    targetId ObjectId
+    - (ref: Task when targetType: 'task')
+    - (ref: Message when targetType: 'message')
+    - (ref: List when targetType: 'list')
+    */
     targetType: { type: String, enum: ['task', 'message', 'list'] },
     details: { type: String },
   },
