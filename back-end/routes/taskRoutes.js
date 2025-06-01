ed925d73f2a3caad34134d06af7af529fa74dfa7
Trinh Nguyen Ngoc Anh QP3029 <anhtnnhe171240@fpt.edu.vn>
2025-06-01 16:43:58 +0700

    sendMail task

--- back-end/routes/taskRoutes.js
+++ back-end/routes/taskRoutes.js
@@ -6,7 +6,7 @@ const { protect } = require('../utils/auth');
 router.get('/', taskController.getAllTask);
 router.get('/:id', taskController.getTask);
 router.post('/createTask', taskController.createTask);
-router.put('/updateTask/:id', taskController.updateTask);
+router.put('/updateTask/:id', protect, taskController.updateTask);
 router.delete('/deleteTask/:id', taskController.deleteTask);
 
 module.exports = router;
