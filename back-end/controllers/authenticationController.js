71b4a968c29ccae3fff67821289526acdbb64c0e
lvu237it <luuvanvua7k16vt@gmail.com>
2025-06-01 23:46:27 +0700

    Fix authentication controller

--- back-end/controllers/authenticationController.js
+++ back-end/controllers/authenticationController.js
@@ -6,7 +6,7 @@ const sendMail = require('../utils/sendMail');
 
 const signToken = (id) => {
   return jwt.sign({ id }, process.env.JWT_SECRET, {
-    expiresIn: process.env.JWT_EXPIRES_IN, // 90d từ .env
+    expiresIn: process.env.JWT_EXPIRES_IN,
   });
 };
 
