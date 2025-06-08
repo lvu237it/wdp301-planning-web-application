bb73fdea76d58b0824c59d5fd7d77309f1a4cb8f
lvu237it <luuvanvua7k16vt@gmail.com>
2025-06-08 09:44:10 +0700

    Delete something and add logout

--- front-end/src/components/layout/Header.jsx
+++ front-end/src/components/layout/Header.jsx
@@ -1,7 +1,26 @@
 import { FaBell, FaSearch } from 'react-icons/fa';
 import defaultAvatar from '../../../public/images/user-avatar-default.png';
+import { useState } from 'react';
+import { useCommon } from '../../contexts/CommonContext';
 
 const Header = () => {
+  const [showPopover, setShowPopover] = useState(false);
+  const { navigate, logout } = useCommon();
+
+  const handleAvatarClick = () => {
+    setShowPopover(!showPopover);
+  };
+
+  const handleProfileClick = () => {
+    navigate('/profile');
+    setShowPopover(false);
+  };
+
+  const handleLogoutClick = () => {
+    logout();
+    setShowPopover(false);
+  };
+
   return (
     <header className='header'>
       <div className='header-left'>
@@ -15,8 +34,20 @@ const Header = () => {
           <FaBell />
           <span className='notification-badge'>3</span>
         </button>
-        <div className='avatar'>
-          <img src={defaultAvatar} alt='User Avatar' />
+        <div className='avatar-container'>
+          <div className='avatar' onClick={handleAvatarClick}>
+            <img src={defaultAvatar} alt='User Avatar' />
+          </div>
+          {showPopover && (
+            <div className='avatar-popover'>
+              <div className='popover-item' onClick={handleProfileClick}>
+                Profile
+              </div>
+              <div className='popover-item' onClick={handleLogoutClick}>
+                Logout
+              </div>
+            </div>
+          )}
         </div>
       </div>
     </header>
